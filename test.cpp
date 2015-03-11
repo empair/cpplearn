cd9021f6c1820501142b665debcbad1bf41da95d
empair <maheshpai@gmail.com>
2015-03-10 17:03:12 -0700

    Test.cpp checking to github

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//#include<iostream.h>
+main()
+{
+    int i=0;
+    for(i=1;i<=5;i++)
+    {
+        printf("%d: Hello World! \n", i);
+    }
+    getchar();
+    printf("Thanks!");
+}
